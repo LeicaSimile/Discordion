c601216e79f9306ff53e28cc6aa8bcef8f077429
Angelica Catalan <tumthe3@gmail.com>
2017-07-08 13:44:32 -0400

    Add __init__ file

---
+++ discordion/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__title__ = "discordion"
+__author__ = "Angelica Catalan"
+__license__ = "MIT"
+__version__ = "0.1.1"
+
+from chatbot import Bot
+from context import GeneralContext
