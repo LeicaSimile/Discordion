f40d91a93613669d78f7f6a1a47cef2afa6b8710
Angelica Catalan <tumthe3@gmail.com>
2017-07-12 20:32:55 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name="discordion",
+      version="0.1.1",
+      description="",
+      author="Angelica Catalan",
+      packages=["discordion"],
+      license="MIT"
+     )
