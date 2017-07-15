07092d4340c6b8f01dc7594deba3e686af7706fa
Angelica Catalan <tumthe3@gmail.com>
2017-07-15 14:47:26 -0400

    Add first unit test outline

---
+++ test/test_chatbot.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from discordion.chatbot import Bot
+
+
+def test_bot_parse():
+    pass
+
+def test_bot_parse_with_context():
+    pass
+
+def test_bot_parse_with_substitutions():
+    pass
+
