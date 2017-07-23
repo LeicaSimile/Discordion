9908e743e4cbf5f055c7363f2b2f08af09d952c9
Angelica Catalan <tumthe3@gmail.com>
2017-07-23 11:56:01 -0400

    Remove install_requires and dependency_links

--- setup.py
+++ setup.py
@@ -6,13 +6,5 @@ setup(name="discordion",
       description="",
       author="Angelica Catalan",
       packages=["discordion"],
-      install_requires=[
-          "pyliner==1.0",
-          "sqlitereader==1.0",
-          ],
-      dependency_links=[
-          "git+ssh://git@github.com/Tumthe3/Pyliner.git@master#egg=pyliner-1.0",
-          "git+ssh://git@github.com/Tumthe3/SQLite-Reader.git@master#egg=sqlitereader-1.0",
-          ],
       license="MIT"
      )
