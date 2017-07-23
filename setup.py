52cc9b27e65ac309ae99c72a71d5d7aa886d574d
Angelica Catalan <tumthe3@gmail.com>
2017-07-23 11:12:51 -0400

    Add dependency_links for pyliner and sqlitereader

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setup(name="discordion",
       description="",
       author="Angelica Catalan",
       packages=["discordion"],
+      dependency_links=[
+          "https://github.com/Tumthe3/Pyliner.git#egg=pyliner-1.0",
+          "https://github.com/Tumthe3/SQLite-Reader.git#egg=sqlitereader-1.0",
+          ]
       license="MIT"
      )
