ee491211518c12ee17511f08a832a99a2900b8e6
Angelica Catalan <tumthe3@gmail.com>
2017-07-23 11:28:51 -0400

    Remove install_requires

--- setup.py
+++ setup.py
@@ -6,10 +6,6 @@ setup(name="discordion",
       description="",
       author="Angelica Catalan",
       packages=["discordion"],
-      install_requires=[
-          "pyliner==1.0",
-          "sqlitereader==1.0",
-          ],
       dependency_links=[
           "https://github.com/Tumthe3/Pyliner/archive/master.zip#egg=pyliner-1.0",
           "https://github.com/Tumthe3/SQLite-Reader/archive/master.zip#egg=sqlitereader-1.0",
