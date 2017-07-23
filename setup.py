672ee8952fba5d19c545592e36f214251b440fb7
Angelica Catalan <tumthe3@gmail.com>
2017-07-23 12:46:22 -0400

    Add pyliner to install_requires

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup
 setup(name="discordion",
       version="0.1.1",
       description="",
+      install_requires=[
+          "pyliner",
+          ],
       author="Angelica Catalan",
       packages=["discordion"],
       license="MIT"
