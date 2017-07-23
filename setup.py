b3d60c8d3d93371a3a7f2266ab5f58aba8544d4e
Angelica Catalan <tumthe3@gmail.com>
2017-07-23 18:45:46 -0400

    Remove install_requires

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup
 setup(name="discordion",
       version="0.1.1",
       description="",
-      install_requires=[
-          "pyliner",
-          ],
       author="Angelica Catalan",
       packages=["discordion"],
       license="MIT"
