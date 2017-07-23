2e0bb2b18d3be7794d1a87185c41291a4ef19044
Angelica Catalan <tumthe3@gmail.com>
2017-07-23 18:47:24 -0400

    Use username as author, add description, email, url

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup
 
 setup(name="discordion",
       version="0.1.1",
-      description="",
-      author="Angelica Catalan",
+      description="A simple framework for discord chatbots",
+      author="Tumthe3",
+      author_email="tumthe3@gmail.com",
+      url="https://github.com/Tumthe3/Discordion",
       packages=["discordion"],
       license="MIT"
      )
