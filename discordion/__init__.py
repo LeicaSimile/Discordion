509be787b5bab5a94d9b2cf62598096eaedafa9a
Angelica Catalan <leicasimile@gmail.com>
2018-05-08 21:21:27 -0400

    Update version to 0.1.2

--- discordion/__init__.py
+++ discordion/__init__.py
@@ -3,7 +3,7 @@
 __title__ = "discordion"
 __author__ = "Angelica Catalan"
 __license__ = "MIT"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .chatbot import Bot
 from .context import GeneralContext
