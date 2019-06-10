5a6648c48ad33199c6e58ecb6755785aa40d1947
Angelica Catalan <leicasimile@gmail.com>
2019-06-10 13:05:31 -0400

    Change url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name="discordion",
       description="A simple framework for discord chatbots",
       author="LeicaSimile",
       author_email="leicasimile@gmail.com",
-      url="https://github.com/LeicaSimile/Discordion",
+      url="https://github.com/LeicaSimile/discordion",
       install_requires=[
           "pyliner==1.0",
           "sqlitehouse==1.2",
