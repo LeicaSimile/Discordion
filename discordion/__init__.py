f4cad15deaf3aee42f511a344a7bf10f1fb983c7
Angelica Catalan <leicasimile@gmail.com>
2019-06-10 21:31:39 -0400

    Remove help command

--- discordion/__init__.py
+++ discordion/__init__.py
@@ -3,7 +3,7 @@
 __title__ = "discordion"
 __author__ = "Angelica Catalan"
 __license__ = "MIT"
-__version__ = "0.1.14"
+__version__ = "0.1.15"
 
 from .chatbot import Bot
 from .context import GeneralContext
--- discordion/chatbot.py
+++ discordion/chatbot.py
@@ -29,10 +29,9 @@ class Bot(object):
         self.file_config = file_config
         settings.read_settings(self.file_config)
         command_prefix = config.get("bot", "prefix")
-        help_command = config.get("bot", "help_command")
         description = config.get("bot", "description")
 
-        self.client = discord.ext.commands.Bot(command_prefix=command_prefix, help_command=help_command, description=description, **options)
+        self.client = discord.ext.commands.Bot(command_prefix=command_prefix, description=description, **options)
         self.db_manual = sqlitehouse.Database(config.get("files", "database_manual"))
         self.db_auto = sqlitehouse.Database(config.get("files", "database_auto"))
 
