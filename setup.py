683110a6798659b136e186cf0eb2ae0fc102a174
Angelica Catalan <leicasimile@gmail.com>
2019-09-14 22:52:51 -0400

    Update pyliner egg

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 from setuptools import setup
 
 setup(name="discordion",
-      version="0.1.20",
+      version="0.1.21",
       description="A simple framework for discord chatbots",
       author="LeicaSimile",
       author_email="leicasimile@gmail.com",
       url="https://github.com/LeicaSimile/discordion",
       install_requires=[
-          "pyliner==1.3",
           "sqlitehouse==1.2",
           ],
       packages=["discordion"],
